ff8bd05d7afda043288d78b539df1c35206caaa0
Syed Hamza Hussain <162815640+HamzaTirmizi@users.noreply.github.com>
2026-07-09 10:48:22 -0700

    Update uploadMiddleware.js

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -16,7 +16,6 @@ const uploadDir = path.join(__dirname, '..', 'uploads', 'documents');
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadDir);
